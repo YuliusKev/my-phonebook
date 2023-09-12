fb9d7ec6022329ec494cf2726913b7ce24bd048c
Yulius <juliussiregar14@gmail.com>
2023-09-12 09:54:41 +0700

    Can add number now

--- src/Components/InputContact/InputContact.tsx
+++ src/Components/InputContact/InputContact.tsx
@@ -1,21 +1,46 @@
 /** @jsxImportSource @emotion/react */
 import { Button, FormGroup, TextField } from '@mui/material';
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import * as styles from  "./InputContact.style.ts"
+import { useMutation } from '@apollo/client';
+import  { ADD_CONTACT_MUTATIONS } from '../../GraphQL/Mutations.tsx'
 
   
 function InputContactForm() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [phone, setPhone] = useState(Number);
+
+    const [AddContactWithPhones, { error }] = useMutation(ADD_CONTACT_MUTATIONS)
+
+    const addContact = () => {
+        AddContactWithPhones({
+            variables: {
+                "first_name": firstName,
+                "last_name" : lastName,
+                "phones": [
+                    {
+                        "number":phone
+                    },
+                ]
+            }
+        })
+
+        if(error) {
+            console.log(error);
+        }
+    }
     return (
         <div>
             <h1 css={styles.title}>
                 Masukkan Contact
             </h1>
             <FormGroup>
-                <TextField label="Nama Depan" variant="outlined" css={styles.input}/>
-                <TextField label="Nama Belakang" variant="outlined" css={styles.input}/>
-                <TextField label="Nomor Telepon" variant="outlined" type='number'  css={styles.input}/>
+                <TextField label="Nama Depan" variant="outlined" onChange={e => setFirstName(e.target.value)} css={styles.input}/>
+                <TextField label="Nama Belakang" variant="outlined" onChange={e => setLastName(e.target.value)} css={styles.input}/>
+                <TextField label="Nomor Telepon" variant="outlined" type='number' onChange={e => setPhone(Number(e.target.value))} css={styles.input}/>
                 <Button variant="outlined" css={styles.button}>Tambah Nomor Telepon</Button>
-                <Button variant="contained" css={styles.button}>Masukkan Kontak</Button>
+                <Button variant="contained" onClick={() => addContact()} css={styles.button}>Masukkan Kontak</Button>
             </FormGroup>
         </div>
     );
--- src/GraphQL/Mutations.tsx
+++ src/GraphQL/Mutations.tsx
@@ -1,3 +1,30 @@
 import {gql} from "@apollo/client"
 
-export default {}
+
+export const ADD_CONTACT_MUTATIONS = gql`
+    mutation AddContactWithPhones(
+        $first_name: String!, 
+        $last_name: String!, 
+        $phones: [phone_insert_input!]!
+        ) {
+        insert_contact(
+            objects: {
+                first_name: $first_name, 
+                last_name: 
+                $last_name, phones: { 
+                    data: $phones
+                    }
+                }
+            ) {
+            returning {
+                first_name
+                last_name
+                id
+                phones {
+                    number
+                }
+            }
+        }
+    }
+
+`;
