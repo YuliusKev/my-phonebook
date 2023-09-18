f4483e1114801011cec3d949d745cd8a51068bab
Yulius <juliussiregar14@gmail.com>
2023-09-18 11:37:14 +0700

    set link to using router-dom

--- src/Components/ContactHeader.tsx
+++ src/Components/ContactHeader.tsx
@@ -1,46 +1,54 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Divider, IconButton } from "@mui/material";
+import { Divider, IconButton, Link } from "@mui/material";
 import { useLocation, useParams } from "react-router-dom";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import AddIcon from "@mui/icons-material/Add";
+import { Link as RouterLink } from "react-router-dom";
 
 export default function ContactHeader() {
   const route = useLocation();
   const params = useParams();
-  console.log(params)
+  console.log(params);
   return (
     <>
       <AppBar position="static">
         <Toolbar>
-          {(route.pathname.startsWith("/input-contact") || route.pathname.startsWith("/edit-contact")) && (
-            <IconButton
-              size="large"
-              edge="start"
-              color="default"
-              aria-label="menu"
-              sx={{ color: "#fff" }}
-              href="/"
-            >
-              <ArrowBackIosNewIcon />
-            </IconButton>
+          {(route.pathname.startsWith("/input-contact") ||
+            route.pathname.startsWith("/edit-contact")) && (
+            <Link component={RouterLink} to={"/"}>
+              <IconButton
+                size="large"
+                edge="start"
+                color="default"
+                aria-label="menu"
+                sx={{ color: "#fff" }}
+              >
+                <ArrowBackIosNewIcon />
+              </IconButton>
+            </Link>
           )}
 
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {route.pathname.startsWith("/input-contact") ? "Add Contact" : (route.pathname.startsWith("/edit-contact")) ? "Edit Contact" : "Contact List"}
+            {route.pathname.startsWith("/input-contact")
+              ? "Add Contact"
+              : route.pathname.startsWith("/edit-contact")
+              ? "Edit Contact"
+              : "Contact List"}
           </Typography>
 
           {route.pathname === "/" && (
-            <IconButton
-              size="large"
-              edge="start"
-              aria-label="menu"
-              sx={{ color: "#fff" }}
-              href="/input-contact"
-            >
-              <AddIcon />
-            </IconButton>
+            <Link component={RouterLink} to={"/input-contact"}>
+              <IconButton
+                size="large"
+                edge="start"
+                aria-label="menu"
+                sx={{ color: "#fff" }}
+              >
+                <AddIcon />
+              </IconButton>
+            </Link>
           )}
         </Toolbar>
       </AppBar>
